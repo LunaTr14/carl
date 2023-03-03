9419be913260f46e03b74305b0c9dc2737b5ad87
Luna T <luna_trn@tutanota.com>
2023-03-03 11:30:10 +1100

    Created JsonParser and TestJsonParser Class
    Author: Luna
    
    - Consolidated json manipulation functions into a class
    - Created a test class for JsonParser

--- main.py
+++ main.py
@@ -14,6 +14,67 @@ CONTENT_FILE = os.path.abspath("./data.json")
 
 def get_token(config_file : str) -> str:
     json_file = open(config_file,mode="r")
+class TestJsonParser():
+    __test_file = os.path.abspath("./test/test.json")
+    __json_parser = None
+    __template_json = {"test": {"read": "9649718a1cd61179343786855795b9c75269d97016f752f697e52e7d0d4141fa",
+                                "write": ""}}
+    __write_answer = "32539bab79202b64f4cfe9dff1288d77f8311ab8934ad9b963679750aca7d238"
+    __read_answer = "9649718a1cd61179343786855795b9c75269d97016f752f697e52e7d0d4141fa"
+
+    def __init__(self) -> None:
+        self.__json_parser = JsonParser(self.__test_file)
+        self.__reset_json()
+
+    def __reset_json(self):
+        json_file = open(self.__test_file, mode="w")
+        dump(self.__template_json, json_file)
+        json_file.close()
+
+    def test_read(self):
+        result = self.__json_parser.read_content()["test"]["read"]
+        assert result == self.__read_answer, self.__print_err(
+            self.__read_answer, result)
+
+    def test_write(self):
+        content = self.__json_parser.read_content()
+        content["test"]["write"] = "32539bab79202b64f4cfe9dff1288d77f8311ab8934ad9b963679750aca7d238"
+        self.__json_parser.write_content(content)
+        result = self.__json_parser.read_content()["test"]["write"]
+        assert result == self.__write_answer, self.__print_err(
+            self.__write_answer, result)
+
+    def __print_err(self, expected, result):
+        return "\n>>Expected: {sample}\n>>Got: {result}".format(sample=expected, result=result)
+
+
+class JsonParser():
+    file_path = ""
+    config_file = "./config.json"
+    save_file = "./save.json"
+
+    def __init__(self, path: str) -> None:
+        self.file_path = path
+
+    def read_content(self) -> dict:
+        content_file = open(file=self.file_path, mode="r")
+        data = load(content_file)
+        content_file.close()
+        return data
+
+    def write_content(self, data: dict) -> None:
+        content_file = open(file=self.file_path, mode="w")
+        dump(data, content_file)
+        content_file.close()
+
+    def is_json_valid(file_path: str) -> bool:
+        try:
+            f = open(file_path, mode="r")
+            load(f)
+            f.close()
+            return True
+        except:
+            return False
     token = load(json_file)["token"]
     json_file.close()
     return token
@@ -48,26 +109,6 @@ async def flip_coin(ctx : Context) -> None:
         await ctx.send("HEADS")
     elif (rand == 0):
         await ctx.send("TAILS")
-
-def read_content(file_path : str) -> dict:
-    content_file = open(file=file_path,mode="r")
-    data = load(content_file)
-    content_file.close()
-    return data
-
-def write_content(file_path : str,data : dict) -> None:
-    content_file = open(file=CONTENT_FILE,mode="w")
-    dump(data,content_file)
-    content_file.close()
-
-def is_json_valid(file_path : str) -> bool:
-    try:
-        f = open(file_path,mode="r")
-        load(f)
-        f.close()
-        return True
-    except:
-        return False
 
 if __name__ == "__main__":
     if(not is_json_valid(CONTENT_FILE)):
