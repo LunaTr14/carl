9ce1e508f0d14b6ac9c579fbdc618e58ccd2a4e4
Luna T <luna_trn@tutanota.com>
2023-03-04 21:54:59 +1100

    Moved Json Test to dedicated .py
    Author: Luna - 4/3/2023
    
    Migrated Json Test from ./main.py to ./test/json-test.py

--- main.py
+++ main.py
@@ -14,70 +14,33 @@ CONTENT_FILE = os.path.abspath("./data.json")
 
 def get_token(config_file : str) -> str:
     json_file = open(config_file,mode="r")
-class TestJsonParser():
-    __test_file = os.path.abspath("./test/test.json")
-    __json_parser = None
-    __template_json = {"test": {"read": "9649718a1cd61179343786855795b9c75269d97016f752f697e52e7d0d4141fa",
-                                "write": ""}}
-    __write_answer = "32539bab79202b64f4cfe9dff1288d77f8311ab8934ad9b963679750aca7d238"
-    __read_answer = "9649718a1cd61179343786855795b9c75269d97016f752f697e52e7d0d4141fa"
-
-    def __init__(self) -> None:
-        self.__json_parser = JsonParser(self.__test_file)
-        self.__reset_json()
-
-    def __reset_json(self):
-        json_file = open(self.__test_file, mode="w")
-        dump(self.__template_json, json_file)
-        json_file.close()
-
-    def test_read(self):
-        result = self.__json_parser.read_content()["test"]["read"]
-        assert result == self.__read_answer, self.__print_err(
-            self.__read_answer, result)
-
-    def test_write(self):
-        content = self.__json_parser.read_content()
-        content["test"]["write"] = "32539bab79202b64f4cfe9dff1288d77f8311ab8934ad9b963679750aca7d238"
-        self.__json_parser.write_content(content)
-        result = self.__json_parser.read_content()["test"]["write"]
-        assert result == self.__write_answer, self.__print_err(
-            self.__write_answer, result)
-
-    def __print_err(self, expected, result):
-        return "\n>>Expected: {sample}\n>>Got: {result}".format(sample=expected, result=result)
 
 
 class JsonParser():
-    file_path = ""
-    config_file = "./config.json"
-    save_file = "./save.json"
+    _file_path = ""
 
     def __init__(self, path: str) -> None:
-        self.file_path = path
+        self._file_path = path
 
     def read_content(self) -> dict:
-        content_file = open(file=self.file_path, mode="r")
+        content_file = open(file=self._file_path, mode="r")
         data = load(content_file)
         content_file.close()
         return data
 
     def write_content(self, data: dict) -> None:
-        content_file = open(file=self.file_path, mode="w")
+        content_file = open(file=self._file_path, mode="w")
         dump(data, content_file)
         content_file.close()
 
-    def is_json_valid(file_path: str) -> bool:
-        try:
-            f = open(file_path, mode="r")
-            load(f)
-            f.close()
-            return True
-        except:
-            return False
-    token = load(json_file)["token"]
-    json_file.close()
-    return token
+    def _does_path_exist(self,path : str) -> bool:
+        return os.path.exists(path)
+    
+    def _create_json(self) -> None:
+        json_file=open(self._file_path,mode="w")
+        json_file.write("{}")
+
+
 
 @app.command()
 async def sus(ctx : Context) -> None:
