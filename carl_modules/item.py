7d436edaa9640c93cade105782274a6d24f47bfa
Luna T <luna_trn@tutanota.com>
2023-03-13 12:32:49 +1100

    Simplified Interface
    Author: Luna
    
    Simplified public functions in:
    carl_modules/user.py
    carl_modules/item.py

--- carl_modules/item.py
+++ carl_modules/item.py
@@ -1,34 +1,17 @@
 from .entity import CarlEntity
 from .csv_handler import CSV
 class Item(CarlEntity,CSV):
-    __item_id = ""
-    __name = ""
-    __boost = 0
+    __item_csv = CSV
 
-    def __init__(self,item_csv: CSV, item_id :str = "") -> None:
-        if(not item_csv.does_entry_exist("item_id",item_id)):
-            self.__item_id = self.create_id("item")
-            self.__name = ""
-            self.__boost = 0
-        else:
-            item_entry = item_csv.get_entry(item_id)
-            self.__name = item_entry[1]
-            self.__boost = item_entry[2]
-
-    def set_name(self,name : str) ->None:
-        self.__name = name
-
-    def set_booster(self,boost : int) -> None:
-        self.__boost = boost
+    def __init__(self,item_csv: CSV) -> None:
+        self.__item_csv = item_csv
     
-    def get_boost(self) -> int:
-        return self.__boost
-    def get_name(self) -> str:
-        return self.__name
-    def export_row(self) -> None:
-        return [
-            self.__item_id,
-            self.__name,
-            self.__boost
-        ]
-    
+    def __find_item(self,item_id) -> list:
+        for entry in self.__item_csv.search(item_id):
+            if(entry[0] == item_id):
+                return entry
+            
+    def get_item_name(self,item_id : str) -> str:
+        return self.__find_item(item_id)[1]
+    def get_boost(self,item_id : str) -> int:
+        return self.__find_item(item_id)[2]
--- carl_modules/user.py
+++ carl_modules/user.py
@@ -8,37 +8,23 @@ class User(CarlEntity):
     __sus_amount = 0
     __game_id = ""
     __inventory_id= ""
-    
-    def get_sus(self) -> int:
-        return self.__sus_amount
-    
-    def get_inventory_id(self) -> str:
-        return self.__inventory_id
-    def get_game_id(self) -> str:
-        return self.__game_id
-    
+    __user_csv = CSV
+    __inventory_csv = CSV
+
     def get_discord_id(self) -> str:
         return self.__discord_id
 
-    def increment_sus(self) -> None:
+    def add_sus(self) -> None:
         self.__sus_amount = self.__sus_amount + 1
-
-    def get_inventory(self,inventory_csv : CSV) -> Inventory:
-        inventory_row = inventory_csv.get_entry("inventory_id",self.__inventory_id)
-        inv = Inventory()
-        inv.set_values_from_row(inventory_row)
-        return inv
     
-    def get_game(self,game_csv : CSV) -> Game:
-        game_row = game_csv.get_entry("game_id",self.__game_id)
-        game = Game()
-        game.set_values_from_row(game_row)
-        return game
-
-    def save_user(self,user_csv:CSV) -> None:
-        user_csv.remove_duplicates(self.__discord_id)
-        user_csv.append_row([self.__discord_id,self.__sus_amount,self.__game_id,self.__inventory_id])
-
+    def save_entry(self):
+        self.__user_csv.save([
+            self.__discord_id,
+            self.__sus_amount,
+            self.__game_id,
+            self.__inventory_id
+            ])
+    
     def __init__(self,user_csv: CSV, discord_id: str = "") -> None:
         self.__discord_id = discord_id
         if(not user_csv.does_entry_exist(discord_id)):
