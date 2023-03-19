b89fccf97b7630545eb1731bae9496211feadf10
Luna T <luna_trn@tutanota.com>
2023-03-19 12:23:30 +1100

    Adjusted fight result format
    Author: Luna
    
    - Replace "SENDER" and "OPPONENT" to a list based

--- carl_modules/game.py
+++ carl_modules/game.py
@@ -9,26 +9,50 @@ class Game():
     def __calculate_win_chance(self,sender_boost,opponent_boost : int) -> float:
         return int(sender_boost) / (int(sender_boost) + int(opponent_boost))
     
-    def __determine_winner(self, win_chance : float) -> str:
+    def __is_winner_sender(self, win_chance : float) -> bool:
         rand = random()
         if (win_chance > rand):
-            return "SENDER"
-        return "OPPONENT"
+            return True
+        return False
 
     def __init__(self,game_csv :CSV) -> None:
         self.__game_csv = game_csv
 
+    def __calculate_score(self,winner :int, loser :int) -> tuple:
+        if(winner > loser):
+            adjustment_score = round(0.75 * (winner - loser))
+            winner = winner + adjustment_score
+            loser = loser - adjustment_score
+        elif(winner < loser):
+            adjustment_score = round(1.25 * (loser - winner))
+            winner = winner + adjustment_score
+            loser = loser + adjustment_score
+        winner = winner + 100
+        loser = loser - 100
+        return (winner,loser)
+
     def fight(self, sender_row : list,opponent_row : list) -> str:
-        if(int(sender_row[1]) <= 0 or int(opponent_row[1]) <= 0):
+        sender_points = round(float(sender_row[1]))
+        opponent_points = round(float(opponent_row[1]))
+        if(sender_points <= 0 or opponent_points <= 0):
             return "Insufficient Points"
-        win_chance = self.__calculate_win_chance(sender_row[2],opponent_row[2])
-        winner = self.__determine_winner(float(win_chance))
-        if(winner == "SENDER"):
-            sender_row[1] = str(int(sender_row[1]) + self.BASE_SCORE)
-            opponent_row[1] = str(int(opponent_row[1]) - self.BASE_SCORE)
-        else:
-            sender_row[1] = str(int(sender_row[1]) - self.BASE_SCORE)
-            opponent_row[1] = str(int(opponent_row[1]) + self.BASE_SCORE)
+        
+        sender_boost = round(float(sender_row[2]))
+        opponent_boost = round(float(sender_row[2]))
+        win_chance = self.__calculate_win_chance(sender_boost,opponent_boost)
+        is_winner_sender = self.__is_winner_sender(float(win_chance))
+        
+        if(is_winner_sender ):
+            new_score_tuple = self.__calculate_score(sender_points,opponent_points)
+            sender_row[1] = str(new_score_tuple[0])
+            opponent_row[1] = str(new_score_tuple[1])
+            winner_details = sender_row
+        elif(not is_winner_sender):
+            new_score_tuple = self.__calculate_score(opponent_points,sender_points)
+            opponent_row[1] = str(new_score_tuple[0])
+            sender_row[1] = str(new_score_tuple[1])
+            winner_details = opponent_row
+        
         self.__game_csv.save(sender_row)
         self.__game_csv.save(opponent_row)
-        return winner
+        return winner_details
