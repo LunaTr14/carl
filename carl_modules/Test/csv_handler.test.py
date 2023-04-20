a4973331db085755360b92c0ff6f1bdeea5128f0
Luna T <luna_trn@tutanota.com>
2023-04-20 12:54:15 +1000

    Updates to CSV Tests
    Author: Luna
    
    Working Test:
    - Read and Write
    - Append
    - Search

--- carl_modules/Test/csv_handler.test.py
+++ carl_modules/Test/csv_handler.test.py
@@ -9,38 +9,37 @@ from csv_handler import CSV
 class TestCSVHandler(unittest.TestCase):
     TEST_PATH = abspath("./Test/.temp_test/")
     TEST_FILE = "test.file"
-    csv_obj = CSV(TEST_PATH, TEST_FILE)
+    csv_obj = CSV(TEST_FILE,TEST_PATH)
     
-    
-    def testReadWrite(self):
-        expected = [["Test1","Test2","1","2","3"]]
-        self.csv_obj.write(expected[0])
+    def test_read_write(self):
+        expected = [["Test1","Test2","1","2","3"],["Test1a","Testb2","cc1","aa2","a3"]]
+        self.csv_obj.write(expected)
         result = self.csv_obj.read()
-        self.assertEqual(result,expected,"\nreadWrite: " + u'\u274C')
-        print("\nreadWrite: " + u'\u2713')
+        self.assertEqual(result,expected,"\nreadWrite: " + "❌")
+        print("\nreadWrite: " + "✅")
     
-    def testSearch(self):
+    def test_search(self):
         expected = [["Test3","Test4","4","5","6"]]
-        self.csv_obj.write(expected[0])
+        self.csv_obj.write(expected)
         results = self.csv_obj.search("Test3")
-        self.assertEqual(results,expected,"\nsearch: " + u'\u274C')
-        print("\nappend: " + u'\u2713')
+        self.assertEqual(results,expected,"\nsearch: " + "❌")
+        print("\nsearch: " + "✅")
 
-    def testAppend(self):
+    def test_append(self):
         expected = [["Test5","Test6","7","8","9"],["Test7","Test8","10","11","12"]]
-        self.csv_obj.write(expected[0])
-        self.csv_obj.append(expected[1])
+        self.csv_obj.write([expected[0]])
+        self.csv_obj.append([expected[1]])
         results = self.csv_obj.read()
-        self.assertEqual(results,expected,"append: " + u'\u274C')
-        
+        self.assertEqual(results,expected,"append: " + "❌")
+        print("\nappend: " + "✅")
     
-    def testCleanDuplicates(self):
+    def test_clean_duplicates(self):
         expected = ["Test7","Test8","13","14","15"]
-        for i in range(3):
-            self.csv_obj.append(expected)
-        self.csv_obj.cleanDuplicates(expected[0])
+        self.csv_obj.append([expected,expected,expected])
+        self.csv_obj.clean_duplicates()
         results = self.csv_obj.read()
-        self.assertEqual(results,expected,"cleanDuplicates: " + u'\u274C')
+        self.assertEqual(results,expected,"clean_duplicates: " + "❌")
+        print("\clean_duplicates: " + "✅")
 
 if(__name__ =="__main__"):
     print("\n-----START CSV TEST-----\n")
--- carl_modules/csv_handler.py
+++ carl_modules/csv_handler.py
@@ -1,68 +1,51 @@
 from os.path import exists, abspath
-from os import mkdir
-from random import randrange
+from os import makedirs
 import csv
 class CSV():
-    _csv_file = None
     _csv_path = ""
-        
-    def __get_folder_tree(self, path: str) -> list:
-        return path.split("/")
 
-    def __create_missing_files(self) -> None:
-        dir_tree = self.__get_folder_tree(self._csv_path)
-        full_path = ""
-        for folder in dir_tree:
-            full_path = full_path + "/"+folder
-            if(not exists(full_path) and "." not in full_path):
-                mkdir(abspath(full_path))
-        open(self._csv_path,mode="w").close()
-    
-    def __init__(self,csv_path : str) -> None:
-        self._csv_path = csv_path
-        if(not exists(self._csv_path)):
-            self.__create_missing_files()
-        self._csv_file = open(csv_path,mode='r') 
+    def __init__(self,file_name : str, file_path : str) -> None:
+        full_path = file_path + "/" + file_name
+        if(not exists(file_path)):
+            makedirs(file_path)
+        if(not exists(full_path)):
+            open(full_path,mode="w").close()
+        self._csv_path = full_path
 
-    def __set_mode(self, mode : str) -> None:
-        self._csv_file.close()
-        self._csv_file = open(self._csv_path,mode=mode)
-    
-    def __remove_duplicates(self, id : str) -> None:
-        valid_entries = []
-        for row in self.__read_csv():
-            if(row[0] != id):
-                valid_entries.append(row)
-        self.__set_mode("w")
-        self._csv_file.write("")
-        csv_writer = csv.writer(self._csv_file,lineterminator="\r")
-        csv_writer.writerows(valid_entries)
+    def read(self) -> list:
+        data = []
+        with open(self._csv_path,mode="r",encoding="ASCII") as csv_file:
+            reader = csv.reader(csv_file,lineterminator="\n")
+            for row in reader:
+                data.append(row)
+            csv_file.close()
+        return data
 
-    def __read_csv(self) -> list:
-        self.__set_mode("r")
-        csv_reader = csv.reader(self._csv_file)
-        rows = []
-        for row in csv_reader:
-            rows.append(row)
-        return rows
+    def write(self, rows : list):
+        with open(self._csv_path, mode="w",encoding="ASCII") as csv_file:
+            writer = csv.writer(csv_file,lineterminator="\n")
+            writer.writerows(rows)
+            csv_file.close()
 
-    def search(self, search_item : str) ->list:
-        data = self.__read_csv()
-        valid_entries = []
+    def append(self, rows : list) -> None:
+        with open(self._csv_path, mode="a") as csv_file:
+            writer = csv.writer(csv_file,lineterminator="\n")
+            writer.writerows(rows)
+            csv_file.close()
+   
+    def search(self, query : str) -> list:
+        results = []
+        data = self.read()
         for row in data:
-            for cell in row:
-                if(search_item == cell):
-                    valid_entries.append(row)
-        return valid_entries
+            if(query not in results):
+                results.append(row)
+        return results
 
-    def does_entry_exist(self, id : str) -> bool:
-        for n in self.__read_csv():
-            if(str(n[0]) == id):
-                return True
-        return False
-
-    def save(self, data : list) -> None:
-        self.__remove_duplicates(data[0])
-        self.__set_mode("a")
-        csv_writer = csv.writer(self._csv_file,lineterminator="\r")
-        csv_writer.writerow(data)
+    def clean_duplicates(self):
+        data = self.read()
+        results = []
+        for row in data:
+            if(row not in results):
+                results.append(row)
+        
+        self.write(results)
